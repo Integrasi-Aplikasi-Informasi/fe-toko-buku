c20657dbe36f53ba5977ad056705f4770a8f7a4e
fabian4819 <habibfabianfahlesi2004@mail.ugm.ac.id>
2024-06-04 21:25:11 +0700

    add logic and ui for checkout page

---
+++ src/app/checkout/page.jsx
@@ -0,0 +1,37 @@
+'use client';
+import Checkout from "../../components/checkout/checkout";
+import { product }  from "../libs/product";
+import { useEffect } from "react";
+import Image from 'next/image';
+
+export default function Home() {
+  useEffect(() => {
+    // render midtrans snap token
+  }, []);
+
+  return (
+    <>
+      <main className="max-w-xl mx-auto sm:p-16">
+        <div className="flex flex-col">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={250}
+            height={250}
+            className="w-full object-cover"
+          />
+          <div className="border border-gray-100 bg-white p-6">
+            <h3 className="mt-4 text-lg font-medium text-gray-900">
+              {product.name}
+            </h3>
+            <p className="mt-1.5 text-sm text-gray-700">Rp {product.price}</p>
+            <p className="py-4 text-sm text-gray-700 text-justify">
+              {product.description}
+            </p>
+            <Checkout />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
