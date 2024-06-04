542b9f96f9e3d49b68b9ec04b2beab87b3175f7e
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2024-06-04 13:18:18 +0700

    Add placeholder pages for seller side

---
+++ src/app/seller/products/page.tsx
@@ -0,0 +1,7 @@
+export default function ProductsDashboardPage() {
+    return (
+      <main className="min-w-screen min-h-screen flex items-center place-content-center">
+        Welcome to Main Page
+      </main>
+    );
+  }
---
+++ src/components/Seller/SellerProductDashboard.tsx
@@ -0,0 +1,96 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+const SellerProductDashboard = () => {
+
+    const [books, setBooks] = useState([
+        {
+            _id: 0, 
+            pengarang: 'khalid', 
+            judul: 'Cara Melakukan Integrasi Aplikasi',
+            detail: '...',
+        },
+        {
+            _id: 1, 
+            pengarang: 'rangga', 
+            judul: 'Becoming a Mobile Expert 101',
+            detail: '...',
+        }
+    ])
+    return (
+    <div className="font-inter">
+      <div className="my-8 flex w-full justify-end">
+        <a href="/sell/create">
+          <p className="w-fit py-1 px-4  bg-[#1AB53C] text-white cursor-pointer rounded-lg hover:bg-green-400 transition duration-150">
+            Add
+          </p>
+        </a>
+      </div>
+      {/* {ownedBook.length == 0 && (
+        <div className="h-44 grid content-center text-blue font-bold text-center">
+          Anda belum punya buku untuk dijual
+        </div>
+      )} */}
+      {/* {ownedBook.length > 0 && ( */}
+        <table className="table-fixed border-[1px] border-black w-full">
+          <thead className="bg-[#35447C]  ">
+            <tr className="text-white text-left  text-sm font-light">
+              <th className="px-2">Pengarang</th>
+              <th className="border-x-[1px] px-2 border-cream w-[20%]">
+                Judul
+              </th>
+              <th className="border-x-[1px] px-2 border-cream overflow-hidden w-[40%]">
+                Detail
+              </th>
+              <th className="w-[20%] px-2">Aksi</th>
+            </tr>
+          </thead>
+          <tbody className="bg-cream">
+            {books.map((list) => (
+              <tr key={list._id}>
+                <td className="px-2 h-8">{list.pengarang}</td>
+                <td className="px-2 border-x-[1px] border-black truncate . . . ">
+                  {list.judul}
+                </td>
+                <td className="px-2 border-x-[1px] border-black overflow-hidden truncate . . .">
+                  {list.detail}
+                </td>
+                <td className="px-2">
+                  <div className="grid grid-cols-3 gap-2 text-center text-white text-sm">
+                    {/* <a href="/sell/read">
+                      <div
+                        className="bg-[#0E7278] cursor-pointer rounded-md hover:bg-emerald-500 transition duration-150"
+                        onClick={() => chooseBook(list)}
+                      >
+                        Read
+                      </div>
+                    </a> */}
+                    {/* <a href="/sell/update">
+                      <div
+                        className="bg-[#E39807] cursor-pointer rounded-md hover:bg-amber-400 transition duration-150"
+                        onClick={() => chooseBook(list)}
+                      >
+                        Edit
+                      </div>
+                    </a> */}
+                    {/* <a href="/sell/delete">
+                      <div
+                        className="bg-[#E82626] cursor-pointer rounded-md hover:bg-red-400 transition duration-150"
+                        onClick={() => chooseBook(list)}
+                      >
+                        Delete
+                      </div>
+                    </a> */}
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+       {/* )} */}
+    </div>
+    )
+}
+
+export default SellerProductDashboard;
