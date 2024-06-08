fea4972505a0dddcc2045cb417479daccf4c155b
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2024-06-09 00:17:41 +0700

    reroute seller pages

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,16 +2,16 @@ export const NAV_INDEXES = [
     {
         label: "Produk",
         value: "produk" as const,
-        href: '/seller/produk'
+        href: '/seller/products'
     },
     {
         label: "Penjualan",
         value: "penjualan" as const,
-        href: "/seller/penjualan"
+        href: "/seller/sales"
     }, 
     {
         label: "Pesanan",
         value: "pesanan" as const,
-        href: "/seller/pesanan"
+        href: "/seller/orders"
     }
 ]
