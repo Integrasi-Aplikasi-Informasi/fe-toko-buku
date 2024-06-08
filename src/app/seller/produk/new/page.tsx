3b98a4850aa97f3785c571eb8a8cb725cdb9283b
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2024-06-08 10:38:06 +0700

    Include navbar to Produk pages

--- src/app/seller/produk/new/page.tsx
+++ src/app/seller/produk/new/page.tsx
@@ -1,7 +1,13 @@
+import MaxWidthWrapper from "@/components/Seller/MaxWidthWrapper";
+import SellerNavbar from "@/components/Seller/SellerNavbar";
+
 const NewProductPage = () => {
     return (
         <div>
-            
+            <SellerNavbar />
+            <MaxWidthWrapper>
+                <h1 className="my-16">Tambahkan Produk Baru</h1>
+            </MaxWidthWrapper>
         </div>
     )
 }
--- src/app/seller/produk/page.tsx
+++ src/app/seller/produk/page.tsx
@@ -1,11 +1,13 @@
 import MaxWidthWrapper from "@/components/Seller/MaxWidthWrapper";
+import SellerNavbar from "@/components/Seller/SellerNavbar";
 import SellerProductDashboard from "@/components/Seller/SellerProductDashboard";
 
 export default function ProductsDashboardPage() {
     return (
         <>
+            <SellerNavbar />
             <MaxWidthWrapper>
-                <main className="min-w-full min-h-screen flex items-center place-content-center">
+                <main className="min-w-full min-h-screen flex items-center place-content-center ">
                     <SellerProductDashboard />
                 </main>
             </MaxWidthWrapper>
