7f582d9118589b94b299d9718cb224cda18ba7c5
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2024-06-08 12:05:15 +0700

    Update seller dashboard

--- src/components/Seller/SellerProductDashboard.tsx
+++ src/components/Seller/SellerProductDashboard.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react";
+import { formatPrice } from "@/lib/utils";
 
 const SellerProductDashboard = () => {
 
@@ -10,19 +11,25 @@ const SellerProductDashboard = () => {
             pengarang: 'khalid', 
             judul: 'Cara Melakukan Integrasi Aplikasi',
             detail: '...',
+            imgUrl: '#',
+            harga: 199000,
+            stok: '20',
         },
         {
             _id: 1, 
             pengarang: 'rangga', 
             judul: 'Becoming a Mobile Expert 101',
             detail: '...',
+            imgUrl: '#',
+            harga: 299000,
+            stok: '20',
         }
     ])
     return (
     <div className="font-inter">
       <div className="my-8 flex w-full justify-end">
         <a href="/sell/create">
-          <p className="w-fit py-1 px-4  bg-[#1AB53C] text-white cursor-pointer rounded-lg hover:bg-green-400 transition duration-150">
+          <p className="w-fit py-1 px-4  bg-[#35CE8D] text-white cursor-pointer rounded-lg hover:bg-green-400 transition duration-150">
             Add
           </p>
         </a>
@@ -34,27 +41,35 @@ const SellerProductDashboard = () => {
       )} */}
       {/* {ownedBook.length > 0 && ( */}
         <table className="table-fixed border-[1px] border-black w-full">
-          <thead className="bg-[#35447C]  ">
+          <thead className="bg-gray-500"> {/*bg-[#35447C]*/ } 
             <tr className="text-white text-left  text-sm font-light">
-              <th className="px-2">Pengarang</th>
-              <th className="border-x-[1px] px-2 border-cream w-[20%]">
+              <th className="px-2 w-[15%]">Gambar</th>
+              <th className="border-x-[1px] px-2 overflow-hidden w-[25%]">
                 Judul
               </th>
-              <th className="border-x-[1px] px-2 border-cream overflow-hidden w-[40%]">
-                Gambar
+              <th className="border-x-[1px] px-2 overflow-hidden w-[23%]">
+                Pengarang
               </th>
-              <th className="w-[20%] px-2">Aksi</th>
+              <th className="w-[7%] px-2 border-x-[1px]">Stok</th>
+              <th className="w-[17%] px-2 border-x-[1px]">Harga</th>
+              <th className="w-[13%] px-2">Aksi</th>
             </tr>
           </thead>
           <tbody className="bg-cream">
             {books.map((list) => (
               <tr key={list._id}>
-                <td className="px-2 h-8">{list.pengarang}</td>
-                <td className="px-2 border-x-[1px] border-black truncate . . . ">
+                <td className="px-2 h-8">{list.imgUrl}</td>
+                <td className="px-2 border-x-[1px] border-black truncate">
                   {list.judul}
                 </td>
-                <td className="px-2 border-x-[1px] border-black overflow-hidden truncate . . .">
-                  {list.detail}
+                <td className="px-2 border-x-[1px] border-black overflow-hidden truncate">
+                  {list.pengarang}
+                </td>
+                <td className="px-2 border-x-[1px] border-black overflow-hidden truncate">
+                  {list.stok}
+                </td>
+                <td className="px-2 border-x-[1px] border-black overflow-hidden truncate">
+                  {formatPrice(list.harga)}
                 </td>
                 <td className="px-2">
                   <div className="grid grid-cols-3 gap-2 text-center text-white text-sm">
@@ -82,7 +97,7 @@ const SellerProductDashboard = () => {
                         Delete
                       </div>
                     </a> */}
-                  </div>
+                  </div> 
                 </td>
               </tr>
             ))}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatPrice(price : number | string) {
+  // Mengonversi harga ke dalam bentuk number jika dalam bentuk string
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  // Menggunakan Intl.NumberFormat untuk memformat harga dalam Rupiah Indonesia
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    maximumFractionDigits: 0  // Tidak ada digit desimal untuk mata uang Rupiah
+  }).format(numericPrice);
+}
