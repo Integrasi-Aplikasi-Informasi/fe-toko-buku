c073c24a40f2bf6194312168f51d274847a0c7f2
khalidrizki01 <khalidrizki01@mail.ugm.ac.id>
2024-06-08 10:36:52 +0700

    Add Seller Navbar

---
+++ src/config/index.ts
@@ -0,0 +1,17 @@
+export const NAV_INDEXES = [
+    {
+        label: "Produk",
+        value: "produk" as const,
+        href: '/seller/produk'
+    },
+    {
+        label: "Penjualan",
+        value: "penjualan" as const,
+        href: "/seller/penjualan"
+    }, 
+    {
+        label: "Pesanan",
+        value: "pesanan" as const,
+        href: "/seller/pesanan"
+    }
+]
