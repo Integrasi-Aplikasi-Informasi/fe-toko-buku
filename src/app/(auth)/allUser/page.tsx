6e8ccebaea573d7569aa3f4b31ba5d835add7359
Dra_faun <faundra.pratamasukma@gmail.com>
2024-06-10 17:13:29 +0700

    Added new UI and handleclick feature

--- src/app/(auth)/allUser/page.tsx
+++ src/app/(auth)/allUser/page.tsx
@@ -1,33 +1,10 @@
-// pages/index.js
-'use client'
-import { useEffect, useState } from 'react';
-import { ref, onValue } from "firebase/database";
-import { database } from '/firebaseConfig';
+import Link from "next/link";
 
-const HomePage = () => {
-  const [usernames, setUsernames] = useState([]);
-
-  useEffect(() => {
-    const usersRef = ref(database, 'users');
-    onValue(usersRef, (snapshot) => {
-      const usersData = snapshot.val();
-      if (usersData) {
-        const usernamesArray = Object.values(usersData).map(user => user.username);
-        setUsernames(usernamesArray);
-      }
-    });
-  }, []);
-
-  return (
+export default function AllUserPage(){
+  return(
     <div>
-      <h1>All Usernames</h1>
-      <ul>
-        {usernames.map((username, index) => (
-          <li key={index}>{username}</li>
-        ))}
-      </ul>
+      Faundra
+      <Link href={"/login"}>Bukan Saya</Link>
     </div>
-  );
-};
-
-export default HomePage;
+  )
+}
