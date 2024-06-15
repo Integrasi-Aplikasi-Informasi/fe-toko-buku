902c641a86a4f89fde8e7aa098b873fd9ba836f0
Adrian463588 <adriansyah2020@mail.ugm.ac.id>
2024-06-15 17:43:21 +0700

    Update track.jsx

--- src/app/ui/dashboard/track/track.jsx
+++ src/app/ui/dashboard/track/track.jsx
@@ -2,3 +2,10 @@ import styles from "./track.module.css";
 
 const Track = () => {
   return (
+    <div className={styles.track}>
+      <h1>Track</h1>
+    </div>
+  );
+};
+
+export default Track;
